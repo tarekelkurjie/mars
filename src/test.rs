7bbc657875be75183a4201fad570abbeee98629e
tarekelkurjie <tarekelkurjie@gmail.com>
2022-03-16 23:55:44 -0700

    yesss v2

--- src/test.rs
+++ src/test.rs
@@ -2,19 +2,17 @@ use std::collections::HashMap;
 
 fn main() {
     unsafe {
-        let mut hash: HashMap<String, &mut Vec<i32>> = HashMap::new();
+        let mut hash: HashMap<String, Vec<i32>> = HashMap::new();
         let mut vec1 = vec![1, 2, 3, 4];
         hash.insert(
             "name".to_string(),
-            &mut vec1
+            vec1
         );
         if let Some(v) = hash.get_mut("name") {
-            if let &mut p = v {
-                let mut vec2 = &mut *p as &mut Vec<i32>;
-                vec2.push(5);
+            let mut vec2 = &mut *p as &mut Vec<i32>;
+            vec2.push(5);
 
-                println!("{:?}", hash.get("name"));
-            }
+            println!("{:?}", hash.get("name"));
         }
 
 
